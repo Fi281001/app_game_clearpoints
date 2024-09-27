6e397ceb537fea3f9836e62097fd7563b29dfd2f
FI281001 <pctrungmagic@gmail.com>
2024-09-27 14:53:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useMemo, useRef } from "react";
+import Swal from "sweetalert2";
 function App() {
   const [value, setValue] = useState(0);
   const [time, setTime] = useState(0);
@@ -8,7 +8,7 @@ function App() {
   const [currentNumber, setCurrentNumber] = useState(1);
   const [clickedNumbers, setClickedNumbers] = useState([]);
   const [isPlaying, setIsPlaying] = useState(true); // Thêm trạng thái để theo dõi
-
+  const valueRef = useRef();
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -21,12 +21,19 @@ function App() {
     const numericValue = value === "" ? 0 : parseInt(value, 10);
     // Kiểm tra điều kiện lỗi
     if (numericValue < 1 || numericValue > 1000) {
-      alert("Giá trị phải nằm trong khoảng từ 1 đến 1000!");
+      Swal.fire({
+        title: "The value must be greater than 1 and less than 1000",
+        text: "Try Agian",
+        confirmButtonText: "OK",
+        allowOutsideClick: false, // Ngăn người dùng đóng modal bằng cách nhấp ngoài
+      });
+      setValue(0);
       setTime(0);
     } else {
       setPoints(
         shuffleArray(Array.from({ length: numericValue }, (_, i) => i + 1))
       );
+
       setIsPlaying(true);
       setTime(10);
     }
@@ -41,18 +48,44 @@ function App() {
     } else {
       setIsPlaying(false); // Đánh dấu là không còn chơi khi hết thời gian
       if (points != 0) {
-        alert("you lose");
-        setValue("");
+        Swal.fire({
+          title: "GAME OVER",
+          text: "Try Agian",
+          confirmButtonText: "OK",
+          allowOutsideClick: false, // Ngăn người dùng đóng modal bằng cách nhấp ngoài
+        });
+        // alert("you lose");
+        setValue(0);
         setIsPlaying(false);
         setPoints([]);
         setClickedNumbers([]);
         setTime(0);
+        valueRef.current.focus();
       }
+      // else {
+      //   alert("you win");
+      // }
     }
 
     // Dọn dẹp interval khi component unmount hoặc time thay đổi
     return () => clearInterval(timer);
   }, [time]);
+
+  useEffect(() => {
+    if (points.length === 0 && time > 0) {
+      Swal.fire({
+        title: "YOU WIN",
+        text: "Congratulations!",
+        confirmButtonText: "OK",
+      });
+      setIsPlaying(false);
+      setPoints([]);
+      setClickedNumbers([]);
+      setValue(0);
+      setTime(0);
+      valueRef.current.focus();
+    }
+  }, [points, time]);
 
   const handleRestart = () => {
     setTime(0);
@@ -60,12 +93,12 @@ function App() {
     setPoints([]);
     setClickedNumbers([]);
     setIsPlaying(false);
+    valueRef.current.focus();
   };
 
   const handleButtonClick = (num) => {
     // check click theo số thứ tự
     if (num === currentNumber) {
-      console.log("số", num);
       setClickedNumbers((prev) => [...prev, num]); // Lưu số đã nhấp
       setCurrentNumber((prev) => prev + 1); // Tăng số hiện tại
       setTimeout(() => {
@@ -91,13 +124,25 @@ function App() {
       ? { backgroundColor: "green", color: "white" }
       : {};
   };
-
+  const randomizedPositions = useMemo(() => {
+    return points.map((num) => {
+      return {
+        num,
+        x: Math.random() * 250,
+        y: Math.random() * 250,
+      };
+    });
+  }, [points]);
   return (
     <div className="App">
       <div>
         <h1>LET'S PLAY</h1>
         <label>Points: </label>
-        <input value={value} onChange={(e) => setValue(e.target.value)} />
+        <input
+          ref={valueRef}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <br />
         <label>Time: </label>
         <span>{time}s</span>
@@ -108,9 +153,9 @@ function App() {
       </div>
 
       <div className="display">
-        {points.map((num) => {
-          const x = Math.random() * 270;
-          const y = Math.random() * 270;
+        {randomizedPositions.map(({ num, x, y }) => {
+          // const x = Math.random() * 250;
+          // const y = Math.random() * 250;
           return (
             <button
               key={num}
